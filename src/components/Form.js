9807301666e38c111b2f33d115cd1dd68fd6829e
Jack Bauer <jackbauer.work@gmail.com>
2023-03-28 12:20:38 -0400

    new commit

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,15 +1,9 @@
 import React from 'react'
-import { Parallax } from "react-parallax";
-import Forms from "../images/welcome.jpg"
-
 
 function Form() {
   return (
-  
-      <Parallax className='form-image' bgImage={Forms} strength={400}>
-    
-      </Parallax>
-    )
+    <div>Form</div>
+  )
 }
 
 export default Form
