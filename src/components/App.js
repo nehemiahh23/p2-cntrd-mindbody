a35ae5c5890f1479bdb0f655ee8f393985464c5a
Nehemiah Henry <nehemiahelihenry@gmail.com>
2023-03-28 10:36:39 -0400

    add base day incrementer

--- src/components/App.js
+++ src/components/App.js
@@ -3,13 +3,23 @@ import Form from "./Form"
 import Mind from "./Mind"
 import Body from "./Body"
 import Connection from "./Connection";
+import moment from "moment";
 
 function App() {
 
   const [submit, setSubmit] = useState(false)
+  const [dateObj, setDateObj] = useState(moment())
+  const [activeDate, setActiveDate] = useState(dateObj.format("L"))
+
+  function dateIncrement() {
+    setDateObj(dateObj.add(1, "days"))
+    setActiveDate(dateObj.format("L"))
+  }
 
   return (
     <>
+      <button onClick={dateIncrement}>New Day</button>
+      <span>{ activeDate }</span>
       <Form submit={submit} setSubmit={setSubmit}/>
       <Mind />
       <Body />
