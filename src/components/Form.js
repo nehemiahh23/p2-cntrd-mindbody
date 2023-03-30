d055a8ab3a056e2b0b96b930dea6d947ee3bbc9b
Nehemiah Henry <nehemiahelihenry@gmail.com>
2023-03-30 10:42:24 -0400

    merge name form with checkin form

--- src/components/Form.js
+++ src/components/Form.js
@@ -3,14 +3,13 @@ import Input from "./Input"
 import Message from "./Message"
 import { Parallax } from "react-parallax";
 import Blue from "../images/blue.jpg"
-import NavBar from "./NavBar"
 
-function Form({ submit, setSubmit, activeDate }) {
+function Form({ submit, setSubmit, activeDate, setUser }) {
 
   return (
     <Parallax className='progress-image' bgImage={Blue} strength={400}>
       <div className='container'>
-      <Input submit={submit} setSubmit={setSubmit} activeDate={activeDate} />
+      <Input submit={submit} setSubmit={setSubmit} activeDate={activeDate} setUser={setUser} />
       <Message submit={submit} />
     </div>
         </Parallax>
