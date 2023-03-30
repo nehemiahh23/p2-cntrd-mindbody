f858fd1a7814292ed192bba823a6124bd43d3c16
Nehemiah Henry <nehemiahelihenry@gmail.com>
2023-03-30 11:10:34 -0400

    uncomment welcome page

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,18 +1,18 @@
-// import React from 'react'
-// import WelcomeImg from '../images/welcome.jpg'
-// import { Parallax } from "react-parallax";
+import React from 'react'
+import WelcomeImg from '../images/welcome.jpg'
+import { Parallax } from "react-parallax";
 
 
 
-// function Welcome({ user }) {
-//   return (
-//       <Parallax className='mind-image' bgImage strength={400}>
-//         {
-//           user ? <h1>Welcome {user.name}</h1> : <h1>Welcome</h1>
-//         }
-//       </Parallax>
-//     )
-// }
+function Welcome({ user }) {
+  return (
+      <Parallax className='mind-image' bgImage strength={400}>
+        {
+          user ? <h1>Welcome {user.name}</h1> : <h1>Welcome</h1>
+        }
+      </Parallax>
+    )
+}
 
 
-// export default Welcome
+export default Welcome
