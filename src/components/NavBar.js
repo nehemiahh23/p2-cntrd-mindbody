c14a30fe2f64846afe359b3e2eeb3db2502af520
Nehemiah Henry <nehemiahelihenry@gmail.com>
2023-03-30 20:40:26 -0400

    navbar finalized

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,5 +1,9 @@
 import { useState, useEffect } from 'react'
 import { NavLink, useNavigate } from 'react-router-dom'
+import MindIcon from "../images/Eco_(24).jpg"
+import BodyIcon from "../images/Health_(23).jpg"
+import ConnectionIcon from "../images/vecteezy_icon-symbol-chat-outline-vector-illustration_.jpg"
+import ProgressIcon from "../images/Statistical__Graph.jpg"
 
 
 function NavBar({ submit }) {
@@ -19,13 +23,15 @@ function NavBar({ submit }) {
   if (submit) {
     return (
         <nav className="nav" style={ appearStyle }>
-          <NavLink to="/ProgressPage">Progress</NavLink>
+          <NavLink to="/MindPage"><img className="icons" src={ MindIcon } alt='Body'></img></NavLink>
           <br />
-          <NavLink to="/MindPage">Mind</NavLink>
+          <NavLink to="/BodyPage">
+            <img className="icons" src={ BodyIcon } alt='Body'></img>
+          </NavLink>
           <br />
-          <NavLink to="/BodyPage">Body</NavLink>
-          <br></br>
-          <NavLink to="/ConnectionPage">Connection</NavLink>
+          <NavLink to="/ConnectionPage"><img className="icons" src={ ConnectionIcon } alt='Body'></img></NavLink>
+          <br />
+          <NavLink to="/ProgressPage"><img className="icons" src={ ProgressIcon } alt='Body'></img></NavLink>
         </nav>
     )
   }
